863ede713fb80b391076b3fb70e4f36f7f10e8d6
musyaffaas <syauqialharaki@gmail.com>
2024-05-25 10:53:22 +0700

    Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../components/Button";
 
 const NotFound = () => {
   return (
-    <div class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div class="text-center">
-        <p class="text-base font-semibold text-indigo-600">404</p>
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Halaman Tidak Tersedia
-        </h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">
-          Maaf, kami tidak dapat menemukan halaman yang kamu cari
-        </p>
-        <div class="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            to={"/"}
-            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </Link>
-        </div>
+    // <div class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center flex flex-col flex-grow p-6 m-14">
+      <p class="text-base font-semibold text-gray-600">404</p>
+      <h1 class="mt-4 text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+        Page Not Found
+      </h1>
+      <p class="mt-6 text-base leading-7 text-gray-600">
+        Maaf, kami tidak dapat menemukan halaman yang kamu cari
+      </p>
+      <div className="flex justify-center py-4">
+        <Button to={"/"} classification="primary" />
       </div>
     </div>
+    // </div>
   );
 };
 
